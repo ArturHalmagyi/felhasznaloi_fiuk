f7ed5816a012868ce212fcfdc15e7e2f8e6d8810
Halmágyi Artúr Tamás <hartur94@gmail.com>
2015-03-26 13:59:58 +0100

    Átneveztem a login ablakot

--- felhasznaloi_fiuk/app/src/main/java/hu/uniobuda/nik/felhasznaloi_fiuk/Guest_Main_menu.java
+++ felhasznaloi_fiuk/app/src/main/java/hu/uniobuda/nik/felhasznaloi_fiuk/Guest_Main_menu.java
@@ -18,7 +18,7 @@ public class Guest_Main_menu extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main_menu, menu);
+        getMenuInflater().inflate(R.menu.menu_guest_main_menu, menu);
         return true;
     }
 
--- felhasznaloi_fiuk/app/src/main/java/hu/uniobuda/nik/felhasznaloi_fiuk/Staff_Main_menu.java
+++ felhasznaloi_fiuk/app/src/main/java/hu/uniobuda/nik/felhasznaloi_fiuk/Staff_Main_menu.java
@@ -18,7 +18,7 @@ public class Staff_Main_menu extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_staff__main_menu, menu);
+        getMenuInflater().inflate(R.menu.menu_staff_main_menu, menu);
         return true;
     }
 
