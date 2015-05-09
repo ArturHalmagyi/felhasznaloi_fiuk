88783753411fbe13436cbd21ae1b814239c62989
Baukristof <baukristof@gmail.com>
2015-05-09 22:47:39 +0200

    comTask comments

--- felhasznaloi_fiuk/app/src/main/java/hu/uniobuda/nik/felhasznaloi_fiuk/Communication.java
+++ felhasznaloi_fiuk/app/src/main/java/hu/uniobuda/nik/felhasznaloi_fiuk/Communication.java
@@ -28,8 +28,8 @@ public class Communication {
     }
 
     public static Random rand = new Random();  //A tesztadatok feltöltéséhez használt random szám generátor
-    static Boolean testMode=false;              //Tesztmód beállitása
-    static boolean staff=true;                  //Személyzet belépve
+    static Boolean testMode;              //Tesztmód beállitása
+    static boolean staff;                  //Személyzet belépve
     private static ComTask ServerCom;
 
     public static ComTask getServerCom() {
